7083d053d3b4aaa9523fd921302c7ff35d39c2d6
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-24 23:04:13 +0530

    Added: Branch uc1 ; Display msg

--- Line_comparison_OOPS.java
+++ Line_comparison_OOPS.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 
 public class Line_comparison_OOPS{
+
+    public static int x1;
+    public static int x2;
+    public static int y1;
+    public static int y2;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program ");
+
+        DisplayMsg.displayMsg();                   //call display method and display welcome msg
+
+        System.out.println(); //for empty space
+
+    }
+
+    //display msg
+    static class DisplayMsg {
+        public static void displayMsg() {
+            System.out.println("Welcome to Line Comparison Computation Program ");
+        }
+
     }
 }
 
